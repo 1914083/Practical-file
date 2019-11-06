9843d4bd8e9f296d8f4f0c7dcce9b76d3fc00334
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:43:11 +0530

    Create sumof2num.c

---
+++ sumof2num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float n1,n2,sum;
+printf("Enter 1st,2nd number:  ");
+scanf("%f%f",&n1,&n2);
+
+sum=n1+n2;
+
+printf("Sum of 1st & 2nd number is = %.2f\n",sum);
+
+return 0;
+}
