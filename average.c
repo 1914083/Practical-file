ce553b63ca904ee74c0a34567cffa85e6f356c44
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:03:02 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a[50];
+float sum=0,avg;
+printf("Enter number of elements:  ");
+scanf("%d",&n);
+printf("Enter %d elements:  ",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+sum+=a[i];
+}
+avg=sum/n;
+printf("Average is %.2f.\n",avg);
+
+return 0;
+}
