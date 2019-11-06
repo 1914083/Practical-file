c5ac7c0667f92d3de50420c31e5b530babdb382c
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:05:45 +0530

    Create circlepattern.c

---
+++ circlepattern.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int r,d,x,y,i,j;
+printf("Enter Radius of circle:  ");
+scanf("%d",&r);
+
+d=2*r+1;    //+1 for including centre
+char a[d][d];
+x=r,y=r;    //Co-ordinates of Centre    
+
+for(i=0;i<d;i++)
+for(j=0;j<d;j++)
+a[i][j]='.';
+
+float dist;
+
+for(i=0;i<d;i++){
+for(j=0;j<d;j++){
+
+dist=sqrt(pow(x-i,2)+pow(y-j,2));
+if(dist>=r-0.5 && dist<=r+0.5)    //dist needs to be only approximately equal to r
+a[i][j]='#';                                                                
+}
+}
+printf("\n\n");
+for(i=0;i<d;i++){
+for(j=0;j<d;j++){
+printf("%c ",a[i][j]);
+}
+printf("\n");
+}
+printf("\n\n");
+
+return 0;
+}
