82f3c5d2aafe674c61c2bdd79aa6156b156c2076
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:20:36 +0530

    Create palindromenum.c

---
+++ palindromenum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>  
+int main()  
+{  
+int rev=0,digit;  
+int n,temp;  
+printf("\nEnter any +ve integer:  ");                                         
+scanf("%d",&n);  
+temp=n;  
+  
+while(temp>0)  
+{                                                                             
+digit=temp%10;    //Stores one's place
+temp/=10;    //Stores number removing one's place
+rev=rev*10+digit;    //Forms reversed number digit-by-digit
+}  
+if(n==rev)                                                                    
+printf("\n%d is a palindrome number.\n",n);  
+else  
+printf("\n%d is not a palindrome number.\n",n);  
+  
+return 0;  
+}
