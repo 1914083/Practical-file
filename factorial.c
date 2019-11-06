5cb3ad13a2fa14d39497c1291c1960ccfafebfde
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:09:25 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,i, fac=1;
+printf("Enter number which you want to get factorial of:  ");
+scanf("%d",&n);
+
+for(i=n;i>1;i--)    //i>0 can also be set as test expression, but multiplying with 1 changes nothing
+fac*=i;
+
+printf("\n%d! = %d\n",n,fac);
+
+return 0;
+}
