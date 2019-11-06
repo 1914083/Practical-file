3ab3719713b1be4ce3b6095088667a7decf727e2
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:45:15 +0530

    Create sumofdigits.c

---
+++ sumofdigits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>                                                             
+int main()              {  
+  
+int sum=0,digit;  
+long n,temp;  
+  
+printf("Enter any +ve integer:  ");   
+scanf("%ld",&n);  
+  
+temp=n;  
+while(temp>0)                                                                 
+{  
+digit=temp%10;    //Stores one's place  
+temp/=10;    //Stores number removing one's place 
+sum+=digit;    //Increments sum digit-by-digit 
+}                                                                             
+  
+printf("\n Sum of digits of %d = %d \n",n,sum);  
+  
+return 0;  
+}
