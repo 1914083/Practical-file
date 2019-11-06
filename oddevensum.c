0678ed2ab487eb9b6de17e9625bc429194dff552
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:20:09 +0530

    Create oddevensum.c

---
+++ oddevensum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  
+int main()              {  
+  
+int esum=0, osum=0, n=1;  
+while(n<=100)  
+{  
+if(n%2==0)  
+esum+=n;  
+  
+else  
+osum+=n;  
+  
+n++;  
+}  
+printf("Sum of even numbers in first 100 +ve integers = %d\n",esum);  
+printf("Sum of odd numbers in first 100 +ve integers = %d\n",osum);  
+  
+return 0;  
+}
