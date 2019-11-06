32f29af7a7834e4ed87139a8f057c6db04b2c45c
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:15:24 +0530

    Create integersum.c

---
+++ integersum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>  
+int main()              {  
+  
+int sum=0,n;  
+n=1;  
+while(n<=100)  
+{  
+sum+=n;  
+n++;  
+}  
+printf("Sum of first 100 +ve integers = %d\n",sum);  
+  
+return 0;  
+}
