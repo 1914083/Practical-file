ee05135229cac9368e02cf9e37f714a905eef8af
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:13:41 +0530

    Create identitymatrix.c

---
+++ identitymatrix.c
@@ -0,0 +1,22 @@
+#include<stdio.h>  
+void main()  
+{  
+int matrix[10][10],sr,sc;  
+printf("Enter the size of Matrix as m,n:  ");  
+scanf("%d%d",&sr,&sc);  
+  
+for(int i=0;i<sr;i++)  
+{  
+printf("\n");  
+for(int j=0;j<sc;j++)  
+{  
+if(i==j)  
+matrix[i][j]=1;    //Displaying 1 at diagonal positions only
+else  
+matrix[i][j]=0;  
+  
+printf("%d ",matrix[i][j]);                                                   
+}  
+}  
+printf("\n\n");  
+}
