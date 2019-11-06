44007965b02876a105b1995cfc114ea2dd7f8b6a
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:39:25 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+int main()  
+{  
+int n,i,res=0;  
+printf("Enter the number you wish to check:  ");  
+scanf("%d",&n);  
+  
+for(i=2;i<=n/2;i++)    //n/2 in test expression to reduce comparisons as each number's factors apart from itself & 1 are from 2 to n/2 only
+{  
+if(n%i==0)  
+res=1;  
+  
+break;  
+}  
+if(res==0)  
+printf("Entered number is Prime.\n");  
+else  
+printf("Entered number is NOT Prime.\n");  
+  
+return 0;  
+}  
