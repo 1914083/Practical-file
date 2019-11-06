3409960de69624de26544026f02332a7fc2a927a
1914083 <53931602+1914083@users.noreply.github.com>
2019-11-06 21:01:55 +0530

    Create armstrongnum.c

---
+++ armstrongnum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+int main()  
+{  
+int n,temp,digit,sum=0;  
+printf("Enter any positive integer:  ");  
+scanf("%d",&n);  
+temp=n;  
+while(temp>0)  
+{  
+digit=temp%10;    //Storing one's place
+temp/=10;    //Storing number removing one's place
+sum+=digit * digit * digit;    //Incrementing sum by cubing & adding all digits one-by-one   
+}  
+if(n==sum)  
+printf("The entered number is an Armstrong number.\n");  
+else  
+printf("The entered number is not an Armstrong number.\n");  
+  
+return 0;  
+}
